3312c54052c5edf5e5bf0e15cdb125167b0f644f
Ahmad sibly <ziblyvila12@gmail.com>
2022-08-31 09:40:24 +0800

    add folder lucky

--- app/admin/undian.php
+++ app/admin/undian.php
@@ -63,7 +63,7 @@
                                              <a type="button" class="btn btn-sm btn-info" href="detail-kelompok.php?id_kelompok=<?= $kelompok['id_kelompok']; ?>">
                                                 <i class="bi bi-eye-fill">Anggota</i>
                                              </a>
-                                             <a href="wheel/index.php?= $kelompok['id_kelompok']; ?>"" class=" btn btn-sm btn-success">
+                                             <a href="lucky/index.php?= $kelompok['id_kelompok']; ?>"" class=" btn btn-sm btn-success">
                                                 <i class="bi bi-pen">Undi</i>
                                              </a>
                                              <!-- modal target -->
